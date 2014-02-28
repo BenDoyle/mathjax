ee37db44972d5ef7b9c0b6d99e7e17af9025d6d9
Ben Doyle <ben.doyle@shopify.com>
2014-02-28 10:26:12 -0500

    more instructions

--- imagemagic_incantation.sh
+++ imagemagic_incantation.sh
@@ -1,1 +1,5 @@
+# download mactex
+# download latexit
+# download imagemagic
+
 convert km_raw.png -transparent white -density 300 -resize 2700 -gravity center -crop 2700x1110+0+0 +repage km_mug.png
