ce89354026f0e0415feb512449be25e0225e8194
Ben Doyle <ben.doyle@shopify.com>
2014-02-28 10:55:38 -0500

    famous equations

--- imagemagic_incantation.sh
+++ imagemagic_incantation.sh
@@ -2,4 +2,6 @@
 # download latexit
 # download imagemagic
 
+# http://www.math.utah.edu/~pa/math/equations/equations.html
+
 convert km_raw.png -transparent white -density 300 -resize 2700 -gravity center -crop 2700x1110+0+0 +repage km_mug.png
